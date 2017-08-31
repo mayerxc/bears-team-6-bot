0a2b64d6c0c8abfa7edd39e614e52b2bd936744f
mbazhlekova <maria.bazhlekova@gmail.com>
2017-08-30 21:54:39 -0500

    Add createPlaylist

--- index.js
+++ index.js
@@ -12,49 +12,53 @@ mongoose.connect('mongodb://localhost:5000/spotifyJukebox');
 mongoose.Promise = global.Promise;
 
 var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 passport.use(new SpotifyStrategy({
     clientID: process.env.SPOTIFY_ID,
     clientSecret: process.env.SPOTIFY_SECRET,
     callbackURL: 'http://dd1bda5c.ngrok.io/auth/spotify/callback',
-    passReqToCallback: true   
-}, function(req, accessToken, refreshToken, profile, done) {
-		process.nextTick(function () {
-			User.findOne({ 'spotifyId': profile.id }, function (err, user) {
-				if (err) {
-					return done(err);
-				}
+    passReqToCallback: true
+}, function (req, accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        User.findOne({
+            'spotifyId': profile.id
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
 
-				if (user) {
-                    user.spotifyToken = accessToken;
+            if (user) {
+                user.spotifyToken = accessToken;
 
-					user.save(function (err) {
-						if (err) {
-							throw err;
-						}
+                user.save(function (err) {
+                    if (err) {
+                        throw err;
+                    }
 
-						return done(null, user);
-					});
-				} else {
-                    var state = JSON.parse(req.query.state);    
-                    
-                    var newUser = new User();
-                    newUser.spotifyId = profile.id;
-                    newUser.spotifyToken = accessToken;
-                    unewUserser = Object.assign(newUser, state.slack);				
+                    return done(null, user);
+                });
+            } else {
+                var state = JSON.parse(req.query.state);
 
-					newUser.save(function (err) {
-						if (err) {
-							throw err;
-						}
+                var newUser = new User();
+                newUser.spotifyId = profile.id;
+                newUser.spotifyToken = accessToken;
+                unewUserser = Object.assign(newUser, state.slack);
 
-						return done(null, newUser);
-					});
-				}
-			});
-		});
+                newUser.save(function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    return done(null, newUser);
+                });
+            }
+        });
+    });
 }));
 
 app.use(session({
@@ -66,27 +70,28 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {    
+passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('<h1>Hello World</h1><br/><a href="/auth/spotify">Spotify Test</a>');
 });
 
-app.get('/success', function(req, res) {
+app.get('/success', function (req, res) {
     res.send('<h1>You have successfully logged in!!</h1>');
 });
 
 // TODO: Does this need to return anything?
 // TODO: factor out find user and not found/log-in message
-app.post('/recentlyPlayed', function (req, res){
-    User.findOne({ 'slackUserId': req.body.user_id }, function (err, user) {
-        console.log(req.body.user_id);
+app.post('/recentlyPlayed', function (req, res) {
+    User.findOne({
+        'slackUserId': req.body.user_id
+    }, function (err, user) {
         if (err) {
             throw err;
         }
@@ -96,86 +101,119 @@ app.post('/recentlyPlayed', function (req, res){
             axios({
                 method: 'get',
                 url: 'https://api.spotify.com/v1/me/player/recently-played',
-                headers: {Authorization: 'Bearer ' + user.spotifyToken}
-            }).then(function(response){ 
+                headers: {
+                    Authorization: 'Bearer ' + user.spotifyToken
+                }
+            }).then(function (response) {
                 var data = [];
-                response.data.items.map(function(item){
+                response.data.items.map(function (item) {
                     data.push({
                         artist: item.track.artists[0].name,
                         album: item.track.album.name,
                         song: item.track.name
                     });
                 });
-                
+
                 axios({
                     method: 'post',
                     url: req.body.response_url,
-                    headers: {'content-type': 'application/json'},
-                    data: {text: JSON.stringify(data)}
-                });                
-            });            
-        } else { 
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    data: {
+                        text: JSON.stringify(data)
+                    }
+                });
+            });
+        } else {
             axios({
                 method: 'post',
                 url: req.body.response_url,
-                headers: {'content-type': 'application/json'},
-                data: {text: 'Please sign up with /login'}
-            });             
+                headers: {
+                    'content-type': 'application/json'
+                },
+                data: {
+                    text: 'Please sign up with /login'
+                }
+            });
         }
     });
-
-
 });
 
-app.post('/createPlaylist', function(req, res) {
-    User.findOne({'slackUserId': req.body.userId}, function(err, user) {
+app.post('/createPlaylist', function (req, res) {
+    User.findOne({
+        'slackUserId': req.body.user_id
+    }, function (err, user) {
         if (err) {
             throw err;
         }
-
+        var playlistName = req.body.team_domain + '_' + req.body.channel_name;
+        console.log(JSON.stringify({name: playlistName, collaborative: true}));
         if (user) {
             axios({
                 method: 'post',
-                url: 'https://api.spotify.com/v1/users/' + req.body.user_id + '/playlists',
-                headers: {Authorization: 'Bearer ' + user.spotifyToken},
-                
-            })
+                url: 'https://api.spotify.com/v1/users/' + user.spotifyId + '/playlists',
+                headers: {
+                    Authorization: 'Bearer ' + user.spotifyToken,
+                    'content-type': 'application/json'
+                },
+                data: JSON.stringify({name: playlistName, collaborative: true})
+            }).then(function (response) {
+                console.log(response);
+            });
+        } else {
+            axios({
+                method: 'post',
+                url: req.body.response_url,
+                headers: {
+                    'content-type': 'application/json'
+                },
+                data: {
+                    text: 'Please sign up with /login'
+                }
+            });
         }
-    })
+    });
 });
 
-app.get('/auth/spotify/',  
-  function(req, res, next){    
-    passport.authenticate('spotify', {
-        scope: ['user-read-email', 'user-read-private', 'user-read-recently-played'],
-        showDialog: true,
-        state: JSON.stringify({
-            slack: {
-                slackUserName: req.query.userName,
-                slackUserId: req.query.userId,
-                slackTeam: req.query.teamName,
-                slackChannel: req.query.channelName,
-            },
-            responseUrl: req.query.responseUrl
-        })
-    })(req, res, next);
-  });  
+app.get('/auth/spotify/',
+    function (req, res, next) {
+        passport.authenticate('spotify', {
+            scope: ['user-read-email', 'user-read-private', 'user-read-recently-played', 'playlist-modify-public', 'playlist-modify-private'],
+            showDialog: true,
+            state: JSON.stringify({
+                slack: {
+                    slackUserName: req.query.userName,
+                    slackUserId: req.query.userId,
+                    slackTeam: req.query.teamName,
+                    slackChannel: req.query.channelName,
+                },
+                responseUrl: req.query.responseUrl
+            })
+        })(req, res, next);
+    });
 
 app.get('/auth/spotify/callback',
-  passport.authenticate('spotify', { failureRedirect: '/' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    var state = JSON.parse(req.query.state);
-    var responseUrl = state.responseUrl;
-    axios({
-        method: 'post',
-        url: responseUrl,
-        headers: {'content-type': 'application/json'},
-        data: {text: 'You have successfully logged in'}
-    }).then(function(response){
-        res.redirect('/success');
-    });       
-  });
+    passport.authenticate('spotify', {
+        failureRedirect: '/'
+    }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        var state = JSON.parse(req.query.state);
+        var responseUrl = state.responseUrl;
+        axios({
+            method: 'post',
+            url: responseUrl,
+            headers: {
+                'content-type': 'application/json'
+            },
+            data: {
+                text: 'You have successfully logged in'
+            }
+        }).then(function (response) {
+            res.redirect('/success');
+        });
+    });
 
 app.post('/login', (req, res) => {
     const url = 'http://dd1bda5c.ngrok.io/auth/spotify?userName=' + req.body.user_name + '&userId=' + req.body.user_id + '&teamName=' + req.body.team_domain + '&channelName=' + req.body.channel_name + '&responseUrl=' + req.body.response_url;
@@ -184,14 +222,11 @@ app.post('/login', (req, res) => {
         'attachments': [{
             'title': 'Login',
             'title_link': url
-        }]    
+        }]
     }
     return res.send(responseObj);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('now listening on port 3000');
 });
-
-
-
